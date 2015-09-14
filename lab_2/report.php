eada04b3f34f8cf6d2152b4e5a65c3265a050a0d
magnoffke <mnoffke@madisoncollege.edu>
2015-09-14 01:41:18 +0000

    finished test drive on pg 42

--- lab_2/report.php
+++ lab_2/report.php
@@ -8,19 +8,22 @@
         <?php
             $when_it_happened = $_POST['whenithappened'];
             $how_long = $_POST['howlong'];
-<<<<<<< HEAD
             $alien_description = $_POST['aliendescription'];
-=======
-            $alien_description = $_POST['description'];
->>>>>>> 0ee8d0a04a2439da881a2234a77b0fe094cd23f2
             $fang_spotted = $_POST['fangspotted'];
             $email = $_POST['email'];
+            $name = $_POST['firstname'] . ' ' . $_POST['lastname'];
+            $how_many = $_POST['howmany'];
+            $what_they_did = $_POST['whattheydid'];
+            $other = $_POST['other'];
             
             echo 'Thanks for submitting the form.<br />';
             echo 'You were abducted ' . $when_it_happened;
             echo ' and were gone for ' . $how_long . '<br />';
+            echo 'Number of aliens: ' . $how_many . '<br />';
             echo 'Describe them: ' . $alien_description . '<br />';
+            echo 'The aliens did this: ' . $what_they_did . '<br />';
             echo 'Was Fang there? ' . $fang_spotted . '<br />';
+            echo 'Other comments: ' . $other . '<br />';
             echo 'Your email address is ' . $email;
             
         ?>
