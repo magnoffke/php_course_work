c2b1aebb52ab8c8becd874900e9e58990a41c5bb
magnoffke <mnoffke@madisoncollege.edu>
2015-09-21 02:07:13 +0000

    finished test drive on page 129

--- lab_3/lab_3.sql
+++ lab_3/lab_3.sql
@@ -3,7 +3,7 @@ CREATE DATABASE elvis_store;
 USE elvis_store;
 
 CREATE TABLE email_list
-    first_name VARCHAR(20),
+   (first_name VARCHAR(20),
     last_name VARCHAR(20),
     email VARCHAR(60)
     );
