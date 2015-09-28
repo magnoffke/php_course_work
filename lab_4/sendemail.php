567c75a19fcf992a1dc5f63aab8748e3b8484ebb
magnoffke <mnoffke@madisoncollege.edu>
2015-09-28 02:08:40 +0000

    finished test drive on page 189

--- lab_4/sendemail.php
+++ lab_4/sendemail.php
@@ -13,26 +13,37 @@
   $subject = $_POST['subject'];
   $text = $_POST['elvismail'];
 
-if ((!empty($subject)) || if(!empty($text))) {
-
-  $dbc = mysqli_connect('localhost', 'magnoffke', '', 'elvis_store')
-    or die('Error connecting to MySQL server.');
-
-  $query = "SELECT * FROM email_list";
-  $result = mysqli_query($dbc, $query)
-    or die('Error querying database.');
-
-  while ($row = mysqli_fetch_array($result)){
-    $to = $row['email'];
-    $first_name = $row['first_name'];
-    $last_name = $row['last_name'];
-    $msg = "Dear $first_name $last_name,\n$text";
-    mail($to, $subject, $msg, 'From:' . $from);
-    echo 'Email sent to: ' . $to . '<br />';
-  } 
-
-  mysqli_close($dbc);
-  
+if ((!empty($subject)) && (!empty($text))) {
+    echo 'You forgot the email subject and body text.<br />';
+}
+else {
+        if (empty($subject) || empty($text)) {
+            if (empty($subject)) {
+                echo 'You forgot the subject line.<br />';
+            } else {
+                echo 'You forgot the email body text. <br />';
+            }
+        } else {
+    
+      $dbc = mysqli_connect('localhost', 'magnoffke', '', 'elvis_store')
+        or die('Error connecting to MySQL server.');
+    
+      $query = "SELECT * FROM email_list";
+      $result = mysqli_query($dbc, $query)
+        or die('Error querying database.');
+    
+      while ($row = mysqli_fetch_array($result)){
+        $to = $row['email'];
+        $first_name = $row['first_name'];
+        $last_name = $row['last_name'];
+        $msg = "Dear $first_name $last_name,\n$text";
+        mail($to, $subject, $msg, 'From:' . $from);
+        echo 'Email sent to: ' . $to . '<br />';
+      } 
+    
+      mysqli_close($dbc);
+      
+    }
   
 }
 ?>
