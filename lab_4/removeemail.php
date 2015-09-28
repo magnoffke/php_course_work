2ec2dfa8e3c5e5df8b3bf993c56f826d8aea1d96
magnoffke <mnoffke@madisoncollege.edu>
2015-09-28 23:39:32 +0000

    finished test drive on page 219

--- lab_4/removeemail.php
+++ lab_4/removeemail.php
@@ -12,16 +12,32 @@
   $dbc = mysqli_connect('data.makemeelvis.com', 'elmer', 'theking', 'elvis_store')
     or die('Error connecting to MySQL server.');
 
-  $email = $_POST['email'];
+  // Delete the customer rows (only if the form has been submitted)
+  if (isset($_POST['submit'])) {
+    foreach ($_POST['todelete'] as $delete_id) {
+      $query = "DELETE FROM email_list WHERE id = $delete_id";
+      mysqli_query($dbc, $query)
+        or die('Error querying database.');
+    } 
 
-  $query = "DELETE FROM email_list WHERE email = '$email'";
-  mysqli_query($dbc, $query)
-    or die('Error querying database.');
+    echo 'Customer(s) removed.<br />';
+  }
 
-  echo 'Customer removed: ' . $email;
+  // Display the customer rows with checkboxes for deleting
+  $query = "SELECT * FROM email_list";
+  $result = mysqli_query($dbc, $query);
+  while ($row = mysqli_fetch_array($result)) {
+    echo '<input type="checkbox" value="' . $row['id'] . '" name="todelete[]" />';
+    echo $row['first_name'];
+    echo ' ' . $row['last_name'];
+    echo ' ' . $row['email'];
+    echo '<br />';
+  }
 
   mysqli_close($dbc);
 ?>
 
+    <input type="submit" name="submit" value="Remove" />
+  </form>
 </body>
 </html>
