57454d78b56f1eb24288f9e1561d742020cf02eb
magnoffke <mnoffke@madisoncollege.edu>
2015-10-04 15:53:39 +0000

    started files for project_1

---
+++ project_1/madlibdisplay.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+        <title>Aliens Abducted Me - Report an Abduction</title>
+    </head>
+    <body>
+        <h2>Aliens Abducted Me - Report an Abduction</h2>
+        
+        <?php
+        
+            $noun = $_POST['noun'];
+            $adjective = $_POST['adjective'];
+            $verb = $_POST['verb'];
+            $adverb = $_POST['adverb'];
+  
+        	$dbc = mysqli_connect('localhost', 'magnoffke', '', 'madlibdatabase')
+        	or die('Error connecting to MySQL server.');
+        	
+        
+        	$query = "INSERT INTO madlib_entries (id, noun, adjective, verb, adverb) " . 
+        		"VALUES ('noun', '$adjective', '$verb', '$adverb')";
+        
+        	$result = mysqli_query($dbc, $query)
+        	 or die('Error querying database.');
+        	 
+        	echo 'Your Madlib:<br /><br />';
+            echo 'Enter a noun: ' . $noun . '<br />';
+            echo 'Enter an adjective: ' . $adjective . '<br />';
+            echo 'Enter a verb: ' . $verb . '<br />';
+            echo 'Enter an adverb: ' . $adverb . '<br /><br /><br />';
+            echo 'The aliens did this: ' . $what_they_did . '<br />';
+            echo 'Your ' . $adjective . $noun .  'likes to '
+            echo $verb $adverb . '. Is that even possible?<br />'
+        
+        	mysqli_close($dbc);
+            
+        ?>
+ 
+    <body>
+
+</html>
