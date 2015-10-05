270adf9e99633a6e7bc21cae96a4cd022d853a21
magnoffke <mnoffke@madisoncollege.edu>
2015-10-05 02:00:57 +0000

    finished test drive on page 265

--- lab_5/index.php
+++ lab_5/index.php
@@ -20,13 +20,18 @@
   $dbc = mysqli_connect('DB_HOST', 'DB_USER', 'DB_PASSWORD', 'DB_NAME');
 
   // Retrieve the score data from MySQL
-  $query = "SELECT * FROM guitarwars";
+  $query = "SELECT * FROM guitarwars ORDER BY score DESC, date ASC";
   $data = mysqli_query($dbc, $query);
 
   // Loop through the array of score data, formatting it as HTML 
   echo '<table>';
+  $i = 0;
   while ($row = mysqli_fetch_array($data)) { 
     // Display the score data
+    if ($i == 0) {
+        echo <tr><td colspan="2" class="topscoreheader">Top Score: ' . 
+        $row['score'] . '</td></tr>';'
+    }
     echo '<tr><td class="scoreinfo">';
     echo '<span class="score">' . $row['score'] . '</span><br />';
     echo '<strong>Name:</strong> ' . $row['name'] . '<br />';
@@ -38,6 +43,7 @@
     else {
       echo '<td><img src="' . GW_UPLOADPATH . 'unverified.gif' . '" alt="Unverified score" /></td></tr>';
     }
+    $i++;
   }
   echo '</table>';
 
