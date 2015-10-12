6a5f94daa06770e7ddffc362c7717fc323c9c685
magnoffke <mnoffke@madisoncollege.edu>
2015-10-12 02:14:56 +0000

    finished test drive on page 311

--- lab_6/admin.php
+++ lab_6/admin.php
@@ -1,3 +1,19 @@
+<?php
+//user name and password for authentication
+  $username = 'rock';
+  $password = 'roll';
+  
+if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW']) ||
+  ($_SERVER['PHP_AUTH_USER'] != $username) || ($_SERVER['PHP_AUTH_PW'] != $password)) {
+    //ther username/password are incorrect so send the authentication headers
+    header('HTTP/1.1 401 Unauthorized');
+    header('WWW-Authenticate: Basic realm="Guitar Wars"');
+    exit('<h2>Guitar Wars</h2>Sorry, you must enter a valid username and password' . 
+      'access this page.');
+  }
+?>
+
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
