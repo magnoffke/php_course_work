92dbe5f454ab5e85d9b931abc65d393c3ea38e8b
magnoffke <mnoffke@madisoncollege.edu>
2015-10-12 02:24:50 +0000

    finished test drive on page 317

--- lab_6/admin.php
+++ lab_6/admin.php
@@ -1,16 +1,5 @@
 <?php
-//user name and password for authentication
-  $username = 'rock';
-  $password = 'roll';
-  
-if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW']) ||
-  ($_SERVER['PHP_AUTH_USER'] != $username) || ($_SERVER['PHP_AUTH_PW'] != $password)) {
-    //ther username/password are incorrect so send the authentication headers
-    header('HTTP/1.1 401 Unauthorized');
-    header('WWW-Authenticate: Basic realm="Guitar Wars"');
-    exit('<h2>Guitar Wars</h2>Sorry, you must enter a valid username and password' . 
-      'access this page.');
-  }
+  require_once('authorize.php');
 ?>
 
 
