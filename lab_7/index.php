c7e18d74d0258ffa34e04976a8d67c6a9454ee6c
magnoffke <mnoffke@madisoncollege.edu>
2015-10-19 02:50:25 +0000

    finished test drive on page 386

--- lab_7/index.php
+++ lab_7/index.php
@@ -1,3 +1,15 @@
+<?php
+  session_start();
+
+  // If the session vars aren't set, try to set them with a cookie
+  if (!isset($_SESSION['user_id'])) {
+    if (isset($_COOKIE['user_id']) && isset($_COOKIE['username'])) {
+      $_SESSION['user_id'] = $_COOKIE['user_id'];
+      $_SESSION['username'] = $_COOKIE['username'];
+    }
+  }
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
@@ -14,8 +26,15 @@
   require_once('connectvars.php');
 
   // Generate the navigation menu
-  echo '&#10084; <a href="viewprofile.php">View Profile</a><br />';
-  echo '&#10084; <a href="editprofile.php">Edit Profile</a><br />';
+  if (isset($_SESSION['username'])) {
+    echo '&#10084; <a href="viewprofile.php">View Profile</a><br />';
+    echo '&#10084; <a href="editprofile.php">Edit Profile</a><br />';
+    echo '&#10084; <a href="logout.php">Log Out (' . $_SESSION['username'] . ')</a>';
+  }
+  else {
+    echo '&#10084; <a href="login.php">Log In</a><br />';
+    echo '&#10084; <a href="signup.php">Sign Up</a>';
+  }
 
   // Connect to the database 
   $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME); 
@@ -34,7 +53,12 @@
     else {
       echo '<tr><td><img src="' . MM_UPLOADPATH . 'nopic.jpg' . '" alt="' . $row['first_name'] . '" /></td>';
     }
-    echo '<td>' . $row['first_name'] . '</td></tr>';
+    if (isset($_SESSION['user_id'])) {
+      echo '<td><a href="viewprofile.php?user_id=' . $row['user_id'] . '">' . $row['first_name'] . '</a></td></tr>';
+    }
+    else {
+      echo '<td>' . $row['first_name'] . '</td></tr>';
+    }
   }
   echo '</table>';
 
@@ -43,3 +67,4 @@
 
 </body> 
 </html>
+
--- lab_7/login.php
+++ lab_7/login.php
@@ -1,39 +1,85 @@
 <?php
   require_once('connectvars.php');
 
-  if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW'])) {
-    // The username/password weren't entered so send the authentication headers
-    header('HTTP/1.1 401 Unauthorized');
-    header('WWW-Authenticate: Basic realm="Mismatch"');
-    exit('<h3>Mismatch</h3>Sorry, you must enter your username and password to log in and access this page. If you ' .
-      'aren\'t a registered member, please <a href="signup.php">sign up</a>.');
+  // Start the session
+  session_start();
+
+  // Clear the error message
+  $error_msg = "";
+
+  // If the user isn't logged in, try to log them in
+  if (!isset($_SESSION['user_id'])) {
+    if (isset($_POST['submit'])) {
+      // Connect to the database
+      $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+      // Grab the user-entered log-in data
+      $user_username = mysqli_real_escape_string($dbc, trim($_POST['username']));
+      $user_password = mysqli_real_escape_string($dbc, trim($_POST['password']));
+
+      if (!empty($user_username) && !empty($user_password)) {
+        // Look up the username and password in the database
+        $query = "SELECT user_id, username FROM mismatch_user WHERE username = '$user_username' AND password = SHA('$user_password')";
+        $data = mysqli_query($dbc, $query);
+
+        if (mysqli_num_rows($data) == 1) {
+          // The log-in is OK so set the user ID and username session vars (and cookies), and redirect to the home page
+          $row = mysqli_fetch_array($data);
+          $_SESSION['user_id'] = $row['user_id'];
+          $_SESSION['username'] = $row['username'];
+          setcookie('user_id', $row['user_id'], time() + (60 * 60 * 24 * 30));    // expires in 30 days
+          setcookie('username', $row['username'], time() + (60 * 60 * 24 * 30));  // expires in 30 days
+          $home_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/index.php';
+          header('Location: ' . $home_url);
+        }
+        else {
+          // The username/password are incorrect so set an error message
+          $error_msg = 'Sorry, you must enter a valid username and password to log in.';
+        }
+      }
+      else {
+        // The username/password weren't entered so set an error message
+        $error_msg = 'Sorry, you must enter your username and password to log in.';
+      }
+    }
   }
+?>
 
-  // Connect to the database
-  $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Mismatch - Log In</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+  <h3>Mismatch - Log In</h3>
 
-  // Grab the user-entered log-in data
-  $user_username = mysqli_real_escape_string($dbc, trim($_SERVER['PHP_AUTH_USER']));
-  $user_password = mysqli_real_escape_string($dbc, trim($_SERVER['PHP_AUTH_PW']));
+<?php
+  // If the session var is empty, show any error message and the log-in form; otherwise confirm the log-in
+  if (empty($_SESSION['user_id'])) {
+    echo '<p class="error">' . $error_msg . '</p>';
+?>
 
-  // Look up the username and password in the database
-  $query = "SELECT user_id, username FROM mismatch_user WHERE username = '$user_username' AND password = SHA('$user_password')";
-  $data = mysqli_query($dbc, $query);
+  <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <fieldset>
+      <legend>Log In</legend>
+      <label for="username">Username:</label>
+      <input type="text" name="username" value="<?php if (!empty($user_username)) echo $user_username; ?>" /><br />
+      <label for="password">Password:</label>
+      <input type="password" name="password" />
+    </fieldset>
+    <input type="submit" value="Log In" name="submit" />
+  </form>
 
-  if (mysqli_num_rows($data) == 1) {
-    // The log-in is OK so set the user ID and username variables
-    $row = mysqli_fetch_array($data);
-    $user_id = $row['user_id'];
-    $username = $row['username'];
+<?php
   }
   else {
-    // The username/password are incorrect so send the authentication headers
-    header('HTTP/1.1 401 Unauthorized');
-    header('WWW-Authenticate: Basic realm="Mismatch"');
-    exit('<h2>Mismatch</h2>Sorry, you must enter a valid username and password to log in and access this page. If you ' .
-      'aren\'t a registered member, please <a href="signup.php">sign up</a>.');
+    // Confirm the successful log-in
+    echo('<p class="login">You are logged in as ' . $_SESSION['username'] . '.</p>');
   }
-
-  // Confirm the successful log-in
-  echo('<p class="login">You are logged in as ' . $username . '.</p>');
 ?>
+
+</body>
+</html>
